5e9ec6435e341af75b6187978253a8fa5117c2e0
Ankit Mahato <ankmahato@gmail.com>
2017-06-28 19:15:45 +0530

    setup script modified for avro module
    Changed dependency from avro to avro-python3.

--- titus/setup.py
+++ titus/setup.py
@@ -45,8 +45,8 @@ setup(name="titus",
       scripts = ["scripts/pfainspector", "scripts/pfachain", "scripts/pfaexternalize", "scripts/pfarandom", "scripts/pfasize"],
       description="Python implementation of Portable Format for Analytics (PFA): producer, converter, and consumer.",
       test_suite="test",
-      install_requires=["avro >= 1.7.6", "ply >= 3.4"],
-      tests_require=["avro >= 1.7.6", "ply >= 3.4", "PyYAML >= 3.10", "numpy >= 1.6.1", "pytz == 2015.4"],
+      install_requires=["avro-python3 >= 1.7.6", "ply >= 3.4"],
+      tests_require=["avro-python3 >= 1.7.6", "ply >= 3.4", "PyYAML >= 3.10", "numpy >= 1.6.1", "pytz == 2015.4"],
       )
 
 ### details of dependencies:
